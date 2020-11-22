4ecd74433dd72602462dcf623e0df51dbf5f3f30
Hugo Ziviani <hugoziviani@hotmail.com>
2020-11-22 07:54:00 -0300

    concurrence, to be tested

--- knapsack.h
+++ knapsack.h
@@ -29,6 +29,7 @@ struct Node
     //            node (including this node)
     // bound ---> Upper makeBound of maximum weight in subtree
     //            of this node/
+    int id;
     int level, profit, bound;
     float weight;
 };
@@ -40,6 +41,23 @@ void printItems(vector<Item> items){
     }
 }
 
+bool verifyConcurrence(int** conflictMatrixReference,vector<Item> &bagSolution,  int actualItemId){
+    // recebe a matriz de concorrencia,
+    // o vetor das solucoes ate o momento
+    // indice do atual elemento a ser inserido.
+    // na primeira ocorrencia de conflito, já retorna o pau moendo
+    if(conflictMatrixReference== nullptr or bagSolution.empty()){
+        return false;//nao existe conflito
+    }
+    for(auto it : bagSolution){
+        //para cada item das solucoes, pegar o id dele e o id passado e verificar na matriz
+        if (conflictMatrixReference[it.id][actualItemId] == 1 or conflictMatrixReference[actualItemId][it.id] == 1){
+            return true;//sim, existe conflito
+        }
+    }
+    return false;//nao existe conflito
+
+}
 // funcao parametro para ordenar o vetor de valores pelo peso
 bool compareItemsByWeight(Item a, Item b){
         double reason1, reason2;
@@ -48,27 +66,27 @@ bool compareItemsByWeight(Item a, Item b){
     return reason1 > reason2;
 }
 
-// Returns makeBound of weight in subtree rooted with u.
+// Returns makeBound of weight in subtree rooted with actualNode.
 // This function mainly uses Greedy solution to find
 // an upper makeBound on maximum weight.
-int makeBound(Node u, int n, int W, vector<Item> arr)
+int makeBound(Node actualNode, int n, int capacity, vector<Item> arr)
 {
     // if weight overcomes the knapsack capacity, return
     // 0 as expected makeBound
-    if (u.weight >= W)
+    if (actualNode.weight >= capacity)
         return 0;
 
     // initialize makeBound on weight by current weight
-    int profit_bound = u.profit;
+    int profit_bound = actualNode.profit;
 
     // start including items from index 1 more to current
     // item index
-    int j = u.level + 1;
-    int totweight = u.weight;
+    int j = actualNode.level + 1;
+    int totweight = actualNode.weight;
 
     // checking index condition and knapsack capacity
     // condition
-    while ((j < n) && (totweight + arr[j].weight <= W))
+    while ((j < n) and (totweight + arr[j].weight <= capacity))
     {
         totweight    += arr[j].weight;
         profit_bound += arr[j].prof;
@@ -78,14 +96,14 @@ int makeBound(Node u, int n, int W, vector<Item> arr)
     // If k is not n, include last item partially for
     // upper makeBound on weight
     if (j < n)
-        profit_bound += (W - totweight) * arr[j].prof /
+        profit_bound += (capacity - totweight) * arr[j].prof /
                         arr[j].weight;
 
     return profit_bound;
 }
 
-// Returns maximum weight we can get with capacity W
-vector<Item> knapsack(int W, vector<Item> &items, int n, int** conflictMatrixReference){
+// Returns maximum weight we can get with capacity capacity
+vector<Item> knapsack(int capacity, vector<Item> &items, int n, int** conflictMatrixReference){
     vector<Item> bagSolution;
 
 // TODO: se nao funcionar mudar o +n
@@ -119,12 +137,15 @@ vector<Item> knapsack(int W, vector<Item> &items, int n, int** conflictMatrixRef
         node.level = root.level + 1;
 
         // Pega o atual item, seu lucro, e atualiza para ser a nova raiz da busca.
+        node.id = items[node.level].id;
         node.weight = root.weight + items[node.level].weight;
         node.profit = root.profit + items[node.level].prof;
 
 
         //Se o lucro for menor que a capacidade e maior que o lucro anterior, atualiza o lucro maximo
-        if (node.weight <= W && node.profit > maxProfit){
+        // TODO: Verificar concorrencia antes de adicionar o item
+        auto concurrence = verifyConcurrence(conflictMatrixReference, bagSolution, node.id);
+        if (node.weight <= capacity and node.profit > maxProfit and !concurrence){
             maxProfit = node.profit;
             bagSolution.push_back(items[node.level]);
         }
@@ -134,7 +155,7 @@ vector<Item> knapsack(int W, vector<Item> &items, int n, int** conflictMatrixRef
 
         // pega o maior valor resultante da poda para decidir
         // e ver se adiciona ou nao na fila de processos
-        node.bound = makeBound(node, n, W, items);
+        node.bound = makeBound(node, n, capacity, items);
 
         // se o resultado da função for maior que o lucro, só coloca na fila por consideracao
         if (node.bound > maxProfit)
@@ -143,11 +164,9 @@ vector<Item> knapsack(int W, vector<Item> &items, int n, int** conflictMatrixRef
         // faça a mesma operacao, sem alocar o item na mochila
         node.weight = root.weight;
         node.profit = root.profit;
-        node.bound = makeBound(node, n, W, items);
+        node.bound = makeBound(node, n, capacity, items);
         if (node.bound > maxProfit){
             queueToProcess.push(node);
-
-
         }
 
     }
